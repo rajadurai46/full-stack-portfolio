65c4842e7713e609bcb7992034b45158d395516f
raja46 <rj46jr@gmail.com>
2025-12-14 11:22:42 +0530

    second

--- port-folio/vite.config.js
+++ port-folio/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'https://full-stack-portfolio-p0km.onrender.com'
     }
   }
 })
