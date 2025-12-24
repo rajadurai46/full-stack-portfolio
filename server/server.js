0d6bd31c8599de0fbf29bd93bfe6167e5e01b5ea
raja46 <rj46jr@gmail.com>
2025-12-24 20:37:59 +0530

    six

--- server/server.js
+++ server/server.js
@@ -17,17 +17,7 @@ app.use(cors());
 // Middlewares
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "*"
-    );
-    res.header("Access-Control-Allow-Methods", "*");
-    next();
-});
 
-app.options("*", cors());
 
 
 
